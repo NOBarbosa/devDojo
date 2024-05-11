96fdd255487878878e5ed2cd3aa19c890bb850e8
Natalia Barbosa <natalia.barbosa@zig.fun>
2024-05-10 22:43:16 -0300

    first commit

---
+++ src/academy/devdojo/maratonajava/introducao/OlaDevDojo.java
@@ -0,0 +1,16 @@
+//todas as classes dentro de pacotes devem começar com o nome de onde essas classes estão localizadas
+
+package academy.devdojo.maratonajava.introducao;
+
+
+//java doc
+
+/**
+ * isso é um comentário <h1>javadoc</h1>
+ */
+
+public class OlaDevDojo {
+    public static void main(String[] args){
+        System.out.println("KA-ME-HA-ME-HAAAAAA");
+    }
+}
---
+++ src/academy/devdojo/maratonajava/introducao/PrimitivesClasses.java
@@ -0,0 +1,23 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class PrimitivesClasses {
+    //psvm
+    public static void main(String[] args) {
+        //int, double, float, char, byte, short, long, boolean
+        int age = 10;
+        double salarioDouble = 2000;
+        double salarioFloat = 2500;
+        byte idadeByte = 10;
+        short idadeShort = 10;
+        long numeroGrande = 100000;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char genero = 'F';
+
+//        System.out.println("idade igual a " + age);
+//        System.out.println("idade igual a " + age);
+//        System.out.println("idade igual a " + age);
+
+
+    }
+}
